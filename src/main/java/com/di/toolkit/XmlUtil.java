726d7fb0def33d2f229a4e37c0c37e968c69a55c
dinglinbo <dinglinbo@hunbasha.com>
2016-12-13 15:10:22 +0800

    xml解析方法

--- src/main/java/com/di/toolkit/XmlUtil.java
+++ src/main/java/com/di/toolkit/XmlUtil.java
@@ -1,6 +1,7 @@
 package com.di.toolkit;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -11,22 +12,111 @@ public class XmlUtil {
 	public static String start = "<![CDATA[";
 	public static String end = "]]>";
 
-	public <T> T toObject(String xml, Class<T> cl) {
-		return null;
+	@SuppressWarnings("unchecked")
+	public static <T> T toObject(String xml, Class<T> cl) {
+		T o = null;
+		if (cl == java.util.Collection.class || cl == java.util.List.class || cl == java.util.ArrayList.class) {
+			List<Object> l = toList(xml);
+			return (T) l;
+		} else if (cl == java.util.Map.class || cl == java.util.HashMap.class) {
+			Map<String, Object> m = toMap(xml);
+			o = (T) m;
+		} else {
+			try {
+				o = cl.newInstance();
+			} catch (InstantiationException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}
+		return o;
 	}
 
-	public Map<String, Object> toMap(String xml) {
-
-		return null;
+	public static Map<String, Object> toMap(String xml) {
+		Map<String, Object> m = new HashMap<>();
+		m.put("element attributes", getAttributes(xml));
+		if (getWrapperValue(xml).indexOf("<") == -1) {
+			m.put(getWrapperName(xml), getWrapperValue(xml));
+			return m;
+		}
+		xml = getWrapperValue(xml);
+		List<String> split = split(xml);
+		for (String s : split) {
+			String value = getWrapperValue(s);
+			String name = getWrapperName(s);
+			if (!value.startsWith("<") && !value.startsWith(start)) {
+				m.put(name, value);
+			} else if (value.startsWith(start)) {
+				m.put(name, replaceEscape(value));
+			} else if (value.startsWith("<") && !isList(value)) {
+				m.put(name, toMap(s));
+			} else if (isList(value)) {
+				m.put(name, toList(value));
+			}
+		}
+		return m;
 	}
 
-	public static List<String> split(String xml) {
+	private static boolean isList(String xml) {
+		String n1 = getWrapperName(xml);
+		if (xml.indexOf("</" + n1 + ">") == -1) {
+			return false;
+		} else if (xml.substring(xml.indexOf("</" + n1 + ">")).indexOf("<" + n1 + ">") > -1) {
+			return true;
+		}
+		return false;
+	}
+
+	private static List<Object> toList(String xml) {
+		List<Object> ls = new ArrayList<>();
+		for (String s : split(xml)) {
+			if (isList(s)) {
+				ls.add(toList(s));
+			} else {
+				ls.add(toMap(s));
+			}
+		}
+		return ls;
+	}
+
+	private static Map<String, String> getAttributes(String xml) {
+		String s = xml.substring(xml.indexOf("<") + 1, xml.indexOf(">"));
+		String[] ss = s.split(" ");
+		if (ss.length == 1) {
+			return null;
+		}
+		Map<String, String> m = new HashMap<>();
+		for (int i = 1; i < ss.length; i++) {
+			String s0 = ss[i].trim();
+			if (!s0.isEmpty()) {
+				m.put(s0.split("=")[0], delQuot(s0.split("=")[1]));
+			}
+		}
+		return m;
+	}
+
+	private static String delQuot(String s) {
+		return s.substring(s.indexOf('"') + 1, s.lastIndexOf('"'));
+	}
+
+	private static String getWrapperName(String xml) {
+		return xml.substring(xml.indexOf("<") > -1 ? (xml.indexOf("<") + 1) : 0,
+				xml.indexOf(">") > -1 ? xml.indexOf(">") : xml.length()).split(" ")[0];
+	}
+
+	private static String getWrapperValue(String xml) {
+		return xml.substring(xml.indexOf(">") > -1 ? (xml.indexOf(">") + 1) : 0,
+				xml.lastIndexOf("<") > -1 ? xml.lastIndexOf("<") : xml.length());
+	}
+
+	private static String replaceEscape(String xml) {
+		return xml.substring(xml.indexOf(start) + 9, xml.lastIndexOf(end));
+	}
+
+	private static List<String> split(String xml) {
 		int i = 0;
 		int left = 0;// <
 		int right = 0;// >
 		int close = 0;// </ left+close=right;
-		// int cSt = 0;// <![CDATA[
-		// int cEnd = 0;// ]]>
 		char[] cs = xml.toCharArray();
 		List<Integer> ls = new ArrayList<Integer>();
 		while (i < cs.length) {
@@ -41,8 +131,6 @@ public class XmlUtil {
 				} else if (xml.indexOf(start, i - 1) == i) {
 					i = xml.indexOf(end, i) + 3;
 					continue;
-					// cSt++;
-					// cEnd++;
 				}
 				break;
 			case '>':
@@ -72,4 +160,5 @@ public class XmlUtil {
 		}
 		return ss;
 	}
+
 }
