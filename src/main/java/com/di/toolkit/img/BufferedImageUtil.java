2f25bb3c50da1fb88cbbbc15b401855d506811b7
di <123929187@qq.com>
2017-02-05 17:05:27 +0800

    bug

--- src/main/java/com/di/toolkit/img/BufferedImageUtil.java
+++ src/main/java/com/di/toolkit/img/BufferedImageUtil.java
@@ -15,12 +15,16 @@ public class BufferedImageUtil {
 	public static final int WHITE_RGB = -1;
 
 	public static Pixel getPixel(BufferedImage img, int x, int y) {
-		Object data = img.getRaster().getDataElements(x, y, null);
-		Pixel p = new Pixel(x, y);
-		p.setRed(img.getColorModel().getRed(data));
-		p.setGreen(img.getColorModel().getGreen(data));
-		p.setBlue(img.getColorModel().getBlue(data));
-		return p;
+		try {
+			Object data = img.getRaster().getDataElements(x, y, null);
+			Pixel p = new Pixel(x, y);
+			p.setRed(img.getColorModel().getRed(data));
+			p.setGreen(img.getColorModel().getGreen(data));
+			p.setBlue(img.getColorModel().getBlue(data));
+			return p;
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return null;
+		}
 	}
 
 	public static void setColor(BufferedImage img, int x, int y, int red, int green, int blue) {
