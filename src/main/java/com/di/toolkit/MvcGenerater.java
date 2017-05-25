2564a7304c12f3513d877a5ebd350225c86204c8
d <d>
2017-05-25 17:53:35 +0800

    mvc generator

--- src/main/java/com/di/toolkit/MvcGenerater.java
+++ src/main/java/com/di/toolkit/MvcGenerater.java
@@ -1,5 +1,7 @@
 package com.di.toolkit;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
@@ -161,8 +163,14 @@ public class MvcGenerater {
 		}
 		return this;
 	}
-
+	
+	private String entityPackage;
+	private String mapperPackage;
+	private String servicePackage;
+	private String controlPackage;
+	
 	public MvcGenerater createEntity(String entityPackage) {
+		this.entityPackage=entityPackage;
 		for (Table t : tables) {
 			Str s = new Str();
 			String className = StringUtil.trimUnderlinedFirstCharUpper(t.getName());
@@ -218,26 +226,291 @@ public class MvcGenerater {
 		}
 		return b;
 	}
-
-	public MvcGenerater createMapper(String pack) {
+	
+	public MvcGenerater createXml(String xmlPath) {
+		for(Table t:tables){
+			Str s = new Str();
+			String className = StringUtil.trimUnderlinedFirstCharUpper(t.getName());
+			className = StringUtil.firstCharUpper(className);
+			s.line("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>");
+			s.line("<!DOCTYPE mapper PUBLIC \"-//mybatis.org//DTD Mapper 3.0//EN\" \"http://mybatis.org/dtd/mybatis-3-mapper.dtd\" >");
+			s.add("<mapper namespace=\"").add(mapperPackage).add(".").add(className).line("Mapper\">");
+			s.line("	<insert id=\"insert\">");
+			s.add("		insert into `").add(t.getName()).line("` (");
+			for (Column c : t.getAllColumns()) {
+				s.add("		`").add(c.getName()).add("`,").newLine();
+			}
+			s.deleteLastChar();
+			s.line("		)values (");
+			for (Column c : t.getAllColumns()) {
+				String s0 = StringUtil.firstCharLower(StringUtil.trimUnderlinedFirstCharUpper(c.getName()));
+				s.add("		#{").add(s0).add("},").newLine();
+			}
+			s.deleteLastChar();
+			s.line("		)").line("	</insert>");
+			s.line("    <update id=\"update\">");
+			s.add("		update `").add(t.getName()).line("` set ");
+			for (Column c : t.getColumns()) {
+				String s0 = StringUtil.firstCharLower(StringUtil.trimUnderlinedFirstCharUpper(c.getName()));
+				s.add("		`").add(c.getName()).add("` = #{").add(s0).add("},").newLine();
+			}
+			s.deleteLastChar();
+			s.line("		WHERE id = #{id}");
+			s.line("	</update>");
+			s.line("    <delete id=\"delete\">");
+			s.add("		update `").add(t.getName()).line("` set");
+			s.line("		`del_flag` =#{DEL_FLAG_DELETE}");
+			s.line("		WHERE id = #{id}").line("	</delete>");
+			s.add("    <select id=\"get\" resultType=\"").add(className).line("\">");
+			s.add("        SELECT * FROM `").add(t.getName());
+			s.line("` WHERE `del_flag` = 0 AND `id` = #{id}").line("    </select>");
+			s.add("    <select id=\"findList\" resultType=\"").add(className).line("\">");
+			s.add("        SELECT * FROM `").add(t.getName());
+			s.line("` WHERE `del_flag` = #{DEL_FLAG_NORMAL} ORDER BY `updated_at` DESC");
+			s.line("    </select>").line("</mapper>");
+			out(path.replaceFirst("java", "resources") + xmlPath + className + "Mapper.xml", s.toString());
+		}	
+		return this;
+	}
+	
+	public MvcGenerater createMapper(String mapperPackage) {
+		this.mapperPackage=mapperPackage;
+		for(Table t:tables){
+			Str s = new Str();
+			String className = StringUtil.trimUnderlinedFirstCharUpper(t.getName());
+			className = StringUtil.firstCharUpper(className);
+			s.add("package ").add(mapperPackage).line(";").newLine();
+			if(mapperBaseClass!=null){
+				s.add("import ").add(mapperBaseClass.getName()).line(";");
+			}
+			s.add("import ").add(entityPackage).add(".").add(className).add(";").newLine().newLine();
+			s.line("/**").add(" * ").line(t.getComment()+"Mapper接口").line(" * @author Generator");
+			s.add(" * @date ").line(new SimpleDateFormat("yyyy-MM-dd hh:mm").format(new Date()));
+			s.line(" */");
+			s.add("public interface ").add(className).add("Mapper");
+			if(mapperBaseClass!=null){
+				s.add(" extends ").add(mapperBaseClass.getSimpleName());
+			}
+			s.line(" {").newLine().add("}");
+			out(path + mapperPackage.replace(".", "/")+ "/" + className + "Mapper.java", s.toString());
+		}
 		return this;
 	}
 
-	public MvcGenerater createService(String pack) {
+	public MvcGenerater createService(String servicePackage) {
+		this.servicePackage=servicePackage;
+		for(Table t:tables){
+			Str s = new Str();
+			String className = StringUtil.trimUnderlinedFirstCharUpper(t.getName());
+			className = StringUtil.firstCharUpper(className);
+			s.add("package ").add(servicePackage).line(";").newLine();
+			if(serviceBaseClass!=null){
+				s.add("import ").add(serviceBaseClass.getName()).line(";");
+			}
+			s.add("import ").add(entityPackage).add(".").add(className).add(";").newLine();
+			s.add("import ").add(mapperPackage).add(".").add(className).add("Mapper;").newLine().newLine();
+			s.line("/**").add(" * ").line(t.getComment()+"service").line(" * @author Generator");
+			s.add(" * @date ").line(new SimpleDateFormat("yyyy-MM-dd hh:mm").format(new Date()));
+			s.line(" */");
+			s.add("public class ").add(className).add("Service");
+			if(serviceBaseClass!=null){
+				s.add(" extends ").add(serviceBaseClass.getSimpleName());
+			}
+			s.line(" {").newLine().add("}");
+			out(path + servicePackage.replace(".", "/") + "/" + className + "Service.java", s.toString());
+		}
 		return this;
 	}
 
-	public MvcGenerater createControl(String pack) {
+	public MvcGenerater createControl(String controlPackage) {
+		this.controlPackage=controlPackage;
+		for(Table t:tables){
+			Str s = new Str();
+			String className = StringUtil.trimUnderlinedFirstCharUpper(t.getName());
+			className = StringUtil.firstCharUpper(className);
+			s.add("package ").add(controlPackage).line(";").newLine();
+			if(controlBaseClass!=null){
+				s.add("import ").add(controlBaseClass.getName()).line(";");
+			}
+			s.add("import ").add(entityPackage).add(".").add(className).add(";").newLine();
+			s.add("import ").add(servicePackage).add(".").add(className).add("Service;").newLine().newLine();
+			s.line("/**").add(" * ").line(t.getComment()+"controller").line(" * @author Generator");
+			s.add(" * @date ").line(new SimpleDateFormat("yyyy-MM-dd hh:mm").format(new Date()));
+			s.line(" */");
+			s.add("public class ").add(className).add("Controller");
+			if(controlBaseClass!=null){
+				s.add(" extends ").add(controlBaseClass.getSimpleName());
+			}
+			s.line(" {").newLine().add("}");
+			out(path + controlPackage.replace(".", "/") + "/" + className + "Controller.java", s.toString());
+		}
 		return this;
 	}
 
-	public MvcGenerater createView(String path) {
+	public MvcGenerater createView(String viewPath) {
+		for(Table t:tables){
+			Str s = new Str();
+			String className = StringUtil.trimUnderlinedFirstCharUpper(t.getName());
+			//list
+			s.line("<div class=\"container-fluid\">");
+			s.line("    <div class=\"row\">");
+			s.line("        <div class=\"col-md-12\">");
+			s.line("            <form class=\"form-inline\" role=\"form\">");
+			s.line("                <input type=\"hidden\" id=\"pageNum\" name=\"pageNum\" value=\"${pageInfo.pageNum}\">");
+			s.line("                <input type=\"hidden\" id=\"pageSize\" name=\"pageSize\" value=\"${pageInfo.pageSize}\">");
+			s.line("                <div class=\"form-group\">");
+			s.line("                    <a type=\"button\" class=\"btn btn-primary\" onclick=\"edit(0)\">新建活动</a>");
+			s.line("                </div>");
+			s.line("            </form>");
+			s.line("        </div>");
+			s.line("    </div>");
+			s.line("    <br/>");
+			s.line("    <div class=\"row\" id=\"_ba\">");
+			s.line("        <div class=\"col-md-12\" id=\"ba\">");
+			s.line("            <table class=\"table table-bordered table-hover\">");
+			s.line("                <thead>");
+			s.line("                <tr>");
+			for(Column c:t.getAllColumns()){
+				s.add("                    <th>").add((c.getRemark()==null||c.getRemark().isEmpty())?c.getName():c.getRemark()).line("</th>");
+			}
+			s.line("                    <th>操作</th>");
+			s.line("                </tr>");
+			s.line("                </thead>");
+			s.line("                <tbody>");
+			s.line("                    <#list pageInfo.list as p>");
+			s.line("                    <tr>");
+			for(Column c:t.getAllColumns()){
+				String cn=StringUtil.firstCharLower(StringUtil.trimUnderlinedFirstCharUpper(c.getName()));
+				if(c.getType().getJava().equals("java.util.Date")){
+					s.add("                        <td>${p.").add(cn).line("?string('yyyy-MM-dd HH:mm:ss')}</td>");
+				}else{
+					s.add("                        <td>${p.").add(cn).line("!}</td>");
+				}
+			}
+			Column key = t.getPrimaryKeys().get(0);
+			String kn=StringUtil.firstCharLower(StringUtil.trimUnderlinedFirstCharUpper(key.getName()));
+			s.add("                        <td><a href=\"#\" class=\"btn btn-default\" onclick=\"edit('${p.").add(kn).line("}')\">编辑</a></td>");
+			s.line("                    </tr>");
+			s.line("                    </#list>");
+			s.line("                </tbody>");
+			s.line("            </table>");
+			s.line("            <div class=\"col-xs-12\">");
+			s.line("                <#if (pageInfo.pageNum>1)>");
+			s.line("                    <a href=\"#\" onclick=\"pageTo('${pageInfo.prePage}')\">上一页</a>");
+			s.line("                <#else>");
+			s.line("                    上一页");
+			s.line("                </#if>");
+			s.line("                <#if (pageInfo.pageNum<pageInfo.pages)>");
+			s.line("                    <a href=\"#\" onclick=\"pageTo('${pageInfo.nextPage}')\">下一页</a>");
+			s.line("                <#else>");
+			s.line("                    下一页");
+			s.line("                </#if>");
+			s.line("                ,当前第${pageInfo.pageNum}页 共 ${pageInfo.total} 项,${pageInfo.pages} 页,每页");
+			s.line("                <select style=\"height: 32px;\" id=\"pgSize\" onchange=\"changePgSize()\">");
+			s.line("                    <option value=\"10\" <#if pageInfo.pageSize==10>selected</#if>>10</option>");
+			s.line("                    <option value=\"20\" <#if pageInfo.pageSize==20>selected</#if>>20</option>");
+			s.line("                    <option value=\"20\" <#if pageInfo.pageSize==50>selected</#if>>50</option>");
+			s.line("                </select> 项 到第 <input type=\"text\"");
+			s.line("                                      style=\"width:48px;height: 32px;\" id=\"pgNum\"> 页");
+			s.line("                <button class=\"btn btn-default\" onclick=\"pageJunp()\">GO</button>");
+			s.line("            </div>");
+			s.line("        </div>");
+			s.line("    </div>");
+			s.line("</div>");
+			s.line("<script>");
+			s.line("    function changePgSize() {");
+			s.line("        $(\"#pageSize\").val($(\"#pgSize\").val());");
+			s.line("        pageTo(1);");
+			s.line("    }");
+			s.line("    function pageJunp() {");
+			s.line("        pageTo($(\"#pgNum\").val());");
+			s.line("    }");
+			s.line("    function pageTo(num) {");
+			s.line("        $(\"#pageNum\").val(num);");
+			s.line("        $(\"#_ba\").load(\"list #ba\", $(\"form\").eq(0).serialize());");
+			s.line("    }");
+			s.line("    function edit(id) {");
+			s.line("        window.location.href = \"edit?id=\" + id;");
+			s.line("    }");
+			s.line("</script>");
+			createFile(path.replaceFirst("java", "webapp") + viewPath+className);
+			out(path.replaceFirst("java", "webapp") + viewPath+className + "/list.ftl", s.toString());
+			//edit
+			s=new Str();
+			s.line("<div class=\"container-fluid\">");
+			s.line("    <div class=\"row\">");
+			s.line("        <div class=\"col-md-12\">");
+			s.line("            <form class=\"form-inline\">");
+			s.line("                <div class=\"form-group\">");
+			s.line("                    <a href=\"#\" class=\"btn btn-info\" onclick=\"history.go(-1)\">返回</a>");
+			s.line("                </div>");
+			s.line("            </form>");
+			s.line("        </div>");
+			s.line("        <div class=\"col-md-12\">");
+			s.line("            <form class=\"form-horizontal\">");
+			s.add("                <input type=\"hidden\" name=\"").add(kn).add("\" value=\"${").add(className).add(".").add(kn).line("!}\">");
+			for(Column c:t.getColumns()){
+				s.line("                <div class=\"form-group\">");
+				String cn=StringUtil.firstCharLower(StringUtil.trimUnderlinedFirstCharUpper(c.getName()));
+				s.add("                    <label class=\"col-sm-2 control-label\" for=\"").add(cn).add("\">");
+				s.add((c.getRemark()==null||c.getRemark().isEmpty())?kn:c.getRemark()).line("</label>");
+				s.line("                    <div class=\"col-sm-6\">");
+				s.add("                        <input id=\"").add(cn).add("\" name=\"").add(cn).add("\" class=\"form-control\" type=\"text\"");
+				s.add(" value=\"${");
+				if(c.getType().getJava().equals("java.util.Date")){
+					s.add("(p.").add(cn).add("?string(\"yyyy-MM-dd hh:mm:ss\"))!");
+				}else{
+					s.add("p.").add(cn).add("!");
+				}
+				s.line("}\">");
+				s.line("                    </div>");
+				s.line("                </div>");
+			}
+			s.line("                <div class=\"form-group\">");
+			s.line("                    <div class=\"col-sm-2\"></div>");
+			s.line("                    <a href=\"#\" class=\"btn btn-primary\" onclick=\"save();\">保存</a>");
+			s.line("                </div>");
+			s.line("            </form>");
+			s.line("        </div>");
+			s.line("    </div>");
+			s.line("</div>");
+			s.line("<script>");
+			s.line("    function save() {");
+			s.line("        var b = true;");
+			s.line("        var msg = \"错误：\";");
+			for(Column c:t.getColumns()){
+				String cn=StringUtil.firstCharLower(StringUtil.trimUnderlinedFirstCharUpper(c.getName()));
+				s.add("        if ($(\"#").add(cn).line("\").val() == \"\") {");
+				s.line("            b = false;");
+				s.add("            msg += \"").add((c.getRemark()==null||c.getRemark().isEmpty())?kn:c.getRemark()).line("不能为空；\";");
+				s.line("        }");
+			}
+			s.line("        if (b) {");
+			s.line("            $.post(\"save\", $(\"form\").eq(0).serialize(), function (data) {");
+			s.line("                if (data.message == \"success\") {");
+			s.line("                    window.location.href = \"list?\";");
+			s.line("                }");
+			s.line("            }, 'json')");
+			s.line("        } else {");
+			s.line("            alert(msg);");
+			s.line("        }");
+			s.line("    }");
+			s.line("</script>");
+			out(path.replaceFirst("java", "webapp") + viewPath+className + "/edit.ftl", s.toString());
+		}
 		return this;
 	}
 
 	private void out(String path, String content) {
 		System.out.println(path);
 		FileUtil.writeToFile(path, content);
+	}
+	
+	private void createFile(String path){
+		File f=new File(path);
+		if(!f.exists()||!f.isDirectory()){
+			f.mkdir();
+		}
 	}
 
 	String path = null;
